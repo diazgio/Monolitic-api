dcabb37699f1d5d7ab7c8a4972b7253a589629c0
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-08-27 17:19:36 -0500

    Add admin, company models and add confings to handle login with other attriubutes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def devise_parameter_sanitizer
+    if resource_class == Company
+      Company::ParameterSanitizer.new(Company, :company, params)
+    else
+      super
+    end
+  end
 end
---
+++ app/sanitizers/company/parameter_sanitizer.rb
@@ -0,0 +1,6 @@
+class Company::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+    permit(:sign_up, keys: [:username, :email])
+  end
+end
